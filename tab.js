33e97bebc38b5c647c5fb7dc52252de5fd3619b8
Jongchan <tolotol@naver.com>
2022-06-10 17:16:21 +0900

    tab을 이용한 반복문 연습

---
+++ tab.js
@@ -0,0 +1,20 @@
+//쉬운거 하나부터 O
+// 기존에 잇는 orange 삭제 ,첫번째 버튼을 누르면 orange 클래스 부착
+// 기존에 잇는 show 삭제, 박스0에 show 클래스 부착
+
+document.querySelectorAll('.tab-button')[0].addEventListener('click' ,function(){
+    const tabButton1 = document.querySelectorAll('.tab-button')[0];
+
+    if(){};
+
+    // document.querySelectorAll('.tab-button')[0].classList.add('orange');
+    // document.querySelectorAll('.tab-content')[0].classList.add('show');
+
+    // document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+    // document.querySelectorAll('.tab-content')[1].classList.remove('show');
+});
+
+// $('.tab-button').eq(0).on('click', function(){
+
+
+// });
