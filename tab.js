db84788f3c1a6e93c7d2aeb8561f8aeacf1023e6
Jongchan <tolotol@naver.com>
2022-06-10 17:52:20 +0900

    제이쿼리로 버튼 기능 구현해보기

--- tab.js
+++ tab.js
@@ -3,29 +3,39 @@
 // 기존에 잇는 show 삭제, 박스0에 show 클래스 부착
 
  
-document.querySelectorAll('.tab-button')[0].addEventListener('click' ,function(){
-   
+// document.querySelectorAll('.tab-button')[0].addEventListener('click' ,function(){
 
-    document.querySelectorAll('.tab-button')[1].classList.remove('orange');
-    document.querySelectorAll('.tab-button')[2].classList.remove('orange');
+//     document.querySelectorAll('.tab-button')[1].classList.remove('orange');
+//     document.querySelectorAll('.tab-button')[2].classList.remove('orange');
     
-    document.querySelectorAll('.tab-content')[1].classList.remove('show');
-    document.querySelectorAll('.tab-content')[2].classList.remove('show');
+//     document.querySelectorAll('.tab-content')[1].classList.remove('show');
+//     document.querySelectorAll('.tab-content')[2].classList.remove('show');
 
-
-    document.querySelectorAll('.tab-button')[0].classList.add('orange');
-    document.querySelectorAll('.tab-content')[0].classList.add('show');
-
-        const tabButton1 = document.querySelectorAll('.tab-button')[0];
-    const tabButton2 = document.querySelectorAll('.tab-button')[1];
-    const tabButton3 = document.querySelectorAll('.tab-button')[2];
-
-    const tabContent = document.querySelectorAll('.tab-content')[0];
-    const tabContent2 = document.querySelectorAll('.tab-content')[1];
-    const tabContent3 = document.querySelectorAll('.tab-content')[2];
-});
-
-// $('.tab-button').eq(0).on('click', function(){
-
+//     document.querySelectorAll('.tab-button')[0].classList.add('orange');
+//     document.querySelectorAll('.tab-content')[0].classList.add('show');
 
 // });
+
+ $('.tab-button').eq(0).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(0).addClass('orange');
+
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(0).addClass('show');
+  });
+
+  $('.tab-button').eq(1).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(1).addClass('orange');
+
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(1).addClass('show');
+  });
+
+  $('.tab-button').eq(2).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(2).addClass('orange');
+
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(2).addClass('show');
+  });
