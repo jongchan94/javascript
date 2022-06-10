ccbbd3bdf751adbfa152670ba9373a20583e6778
Jongchan <tolotol@naver.com>
2022-06-10 18:12:51 +0900

    for문 문법

--- tab.js
+++ tab.js
@@ -16,26 +16,47 @@
 
 // });
 
- $('.tab-button').eq(0).on('click', function(){
+//좋은 관습 : 자주쓰는 셀렉터 변수에 넣어쓰기;
+//var 버튼 = $('.tab-button)
+
+//  $('.tab-button').eq(0).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(0).addClass('orange');
+
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(0).addClass('show');
+//   });
+
+//   $('.tab-button').eq(1).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(1).addClass('orange');
+
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(1).addClass('show');
+//   });
+
+//   $('.tab-button').eq(2).on('click', function(){
+//     $('.tab-button').removeClass('orange');
+//     $('.tab-button').eq(2).addClass('orange');
+
+//     $('.tab-content').removeClass('show');
+//     $('.tab-content').eq(2).addClass('show');
+//   });
+
+  //for 반복문
+  
+//   for (횟수){
+//     console.log('안녕');
+//   }
+
+  // i가 0부터 시작해서 i를 1개씩 더하고 3이 되면 정지
+  // 반복문 안에 있는 변수를 만들 때 항상 let 사용
+  for (let i = 0; i < 3; i++){
+    $('.tab-button').eq(i).on('click', function(){
     $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(0).addClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
 
     $('.tab-content').removeClass('show');
-    $('.tab-content').eq(0).addClass('show');
+    $('.tab-content').eq(i).addClass('show');
   });
-
-  $('.tab-button').eq(1).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(1).addClass('orange');
-
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(1).addClass('show');
-  });
-
-  $('.tab-button').eq(2).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(2).addClass('orange');
-
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(2).addClass('show');
-  });
+  }
